45f0853a10571b25eb75f1ebce8c64976b88474a
sierrapablo <proyectos@sierrapablo.dev>
2025-06-27 14:36:10 +0200

    ✨ add feature : add view source code on the bottom

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,20 @@
 import { JSX } from "react";
 
 export default function Footer(): JSX.Element {
-    return (
-        <footer className="mt-16 text-center text-xs text-gray-500">
-        <p>~ last update: 2025 — version 0.1.1-dev</p>
-        <p>I'm becoming insane at this point...</p>
-      </footer>
-    );
+  return (
+    <footer className="mt-16 text-center text-xs text-gray-500">
+      <p>~ last update: 2025 — version 0.1.2-dev</p>
+      <p>I'm becoming insane at this point...</p>
+      <p>
+        <a
+          href="https://github.com/sierrapablo/sierrapablo-portfolio"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="underline hover:text-amber-500 transition"
+        >
+          view source
+        </a>
+      </p>
+    </footer>
+  );
 }
