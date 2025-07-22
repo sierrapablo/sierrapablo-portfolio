8c62e7abca7f516ad2cab9c9a04b8df624005094
sierrapablo <proyectos@sierrapablo.dev>
2025-07-22 16:41:04 +0200

    ✨ add feature : add typewriter effect to subtitles with dynamic messages

---
+++ src/scripts/typewriter.js
@@ -0,0 +1,36 @@
+const messages = [
+  "Desarrollador de software freelance.",
+  "Automatizo procesos, creo herramientas y despliego y mantengo infraestructuras.",
+  "Explora mi blog para ver mis últimos experimentos.",
+];
+
+function type() {
+  const el = document.getElementById("typed");
+  if (!el) return;
+
+  let messageIndex = 0;
+  let charIndex = 0;
+
+  function _type() {
+    if (charIndex < messages[messageIndex].length) {
+      el.textContent += messages[messageIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(_type, 50);
+    } else {
+      setTimeout(() => {
+        el.textContent = "";
+        charIndex = 0;
+        messageIndex = (messageIndex + 1) % messages.length;
+        _type();
+      }, 2000);
+    }
+  }
+
+  _type();
+}
+
+export function startTyping() {
+  window.addEventListener("DOMContentLoaded", () => {
+    setTimeout(type, 1000);
+  });
+}
