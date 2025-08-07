61e735cf66f1db6907f6633f0fc1b95f565f9685
sierrapablo <proyectos@sierrapablo.dev>
2025-08-07 14:09:12 +0200

    fix prettier

--- src/scripts/typewriter.ts
+++ src/scripts/typewriter.ts
@@ -1,32 +1,32 @@
 const messages: string[] = [
   'Desarrollador de software freelance.',
-  "Automatizo procesos, creo herramientas, despliego y mantengo infraestructuras.",
+  'Automatizo procesos, creo herramientas, despliego y mantengo infraestructuras.',
   'Explora mis proyectos.',
 ];
 
 export function startTyping(): void {
-  const el = document.getElementById('typed')
-  if (!el) return
+  const el = document.getElementById('typed');
+  if (!el) return;
 
-  el.textContent = ''
+  el.textContent = '';
 
-  let messageIndex = 0
-  let charIndex = 0
+  let messageIndex = 0;
+  let charIndex = 0;
 
   function type(): void {
     if (!el) return;
     if (charIndex < messages[messageIndex].length) {
-      el.textContent += messages[messageIndex].charAt(charIndex)
+      el.textContent += messages[messageIndex].charAt(charIndex);
       charIndex++;
-      setTimeout(type, 50)
+      setTimeout(type, 50);
     } else {
       setTimeout(() => {
-        el.textContent = "";
-        charIndex = 0
-        messageIndex = (messageIndex + 1) % messages.length
+        el.textContent = '';
+        charIndex = 0;
+        messageIndex = (messageIndex + 1) % messages.length;
         type();
       }, 2000);
     }
   }
-  setTimeout(type, 1500)
+  setTimeout(type, 1500);
 }
